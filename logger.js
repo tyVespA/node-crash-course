02b99736cbe925440ca21cc59ff693f867996078
tyVespA <migliavacca.marco08@gmail.com>
2023-12-07 22:58:41 +0100

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,15 @@
+const EventEmitter = require("events");
+const uuid = require("uuid");
+
+class Logger extends EventEmitter {
+  log(msg) {
+    // Call event
+    this.emit("message", { id: uuid.v4(), msg: msg });
+  }
+}
+
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called Listener: ", data));
+
+logger.log("Yello");
