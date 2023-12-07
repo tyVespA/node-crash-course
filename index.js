a7a04bdc39d78a3272ead219f301218f06e40d04
tyVespA <migliavacca.marco08@gmail.com>
2023-12-07 22:58:35 +0100

    Create basic server

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("Marco", 102);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 
-person1.greeting();
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "Marco", age: "40" },
+      { name: "Paolo", age: "41" },
+    ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server running on port ", PORT));
